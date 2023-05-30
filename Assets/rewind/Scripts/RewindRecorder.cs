2459b83e3af9435f2c79781131cb9fc108204d17
treething77 <treething77@gmail.com>
2023-05-30 17:57:25 -0400

    Notes on storage
    Notes on storage

--- Assets/rewind/Scripts/RewindRecorder.cs
+++ Assets/rewind/Scripts/RewindRecorder.cs
@@ -15,7 +15,24 @@ namespace ccl.rewind_plugin
 
         public void updateRecording()
         {
-            throw new System.NotImplementedException();
+            //foreach object in scene
+            foreach (IRewindHandler rewindHandler in _rewindScene.RewindHandlers)
+            {
+                //should we record this frame?
+                //if so, record it
+                
+                //when we create the storage, allocate a section of it for each handler
+                //then each handler writes into that
+                //dont store storage details IN the handler, because then that limits us to 1
+                //scene per handler. Can't then have handlers in multiple scenes.
+                
+                //so the details of where the handler writes to is stored in the storage itself
+                //the storage needs to store a map of ID->storage location
+                //the storage location will then store the array of frame data
+                    
+                //write the data to the rewind storage
+                rewindHandler.rewindStore();
+            }
         }
     }
 }
